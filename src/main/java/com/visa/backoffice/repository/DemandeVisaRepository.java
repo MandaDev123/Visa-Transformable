540003c6c26f49261678d7a71c2ea19cf479e2b1
MandaDev123 <ikotokelymanda@gmail.com>
2026-04-28 22:26:20 -0800

    Modification des repository pour la recherche par visa

--- src/main/java/com/visa/backoffice/repository/DemandeVisaRepository.java
+++ src/main/java/com/visa/backoffice/repository/DemandeVisaRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +14,7 @@ public interface DemandeVisaRepository extends JpaRepository<DemandeVisa, Long> 
 
     @Query("SELECT d FROM DemandeVisa d LEFT JOIN FETCH d.pieces dp LEFT JOIN FETCH dp.piece WHERE d.id = :id")
     Optional<DemandeVisa> findByIdWithPieces(@Param("id") Long id);
+
+    @Query("SELECT d FROM DemandeVisa d LEFT JOIN FETCH d.pieces dp LEFT JOIN FETCH dp.piece WHERE d.numeroVisa = :numero OR d.numeroCarteResident = :numero ORDER BY d.dateDemande DESC")
+    List<DemandeVisa> findHistoriqueByNumero(@Param("numero") String numero);
 }
